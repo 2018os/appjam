994931bb1d8e59c99cd9994ccd53f12c733644f5
2018os <tmdduq4536@naver.com>
2018-12-22 21:46:46 +0900

    first commit

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'JWT_SALT' : '~!@#$%^&*()_+Vaccine+_)(*&^%$#@!~',
+    'mongodbUri': 'mongodb://root:kim41516~@ds034807.mlab.com:34807/appjam'
+}
---
+++ databases/User.js
@@ -0,0 +1,12 @@
+const mongoose =require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    id: { type: String, required: true, unique: true },
+    pw: { type: String, required: true },
+    name: { type: String, required: true },
+    isSenior: { type: Boolean, required: true },
+    tag: { type: String, required: true }
+});
+
+module.exports = mongoose.model('user', userSchema);

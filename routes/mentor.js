0d2b538ab526c8312fa6db3181ddc26a73ad1fa7
2018os <tmdduq4536@naver.com>
2018-12-23 03:29:44 +0900

    forth commit

--- routes/mentor.js
+++ routes/mentor.js
@@ -4,7 +4,7 @@ const User = require('../databases/User');
 router.get('/:id', async (req, res) => {
     try {
         const mentor = await User.findOne({ id: req.params.id });
-        res.json({ "mentor": mentor });
+        res.json({ "mentor": { "tag": mentor.tag, "id": mentor.id, "name": mentor.name, "isSenior": mentor.isSenior } });
     } catch (err) {
         const { message } = err;
         res.status(200).json({ "result": { "success": false, message } });
